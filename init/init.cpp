7e34e1bb630cd6c95eefff467e1fc42b816c23b8
LineageOS-MTK <luzejij@binka.me>
2017-07-16 12:52:35 +0300

    init: HACK re-allow services without selinux contexts defined
    Signed-off-by: teja98 <tejachaitu96@gmail.com>

--- init/init.cpp
+++ init/init.cpp
@@ -500,6 +500,7 @@ static void selinux_init_all_handles(void)
     sehandle_prop = selinux_android_prop_context_handle();
 }
 
+#if 0
 enum selinux_enforcing_status { SELINUX_PERMISSIVE, SELINUX_ENFORCING };
 
 static selinux_enforcing_status selinux_status_from_cmdline() {
@@ -513,11 +514,12 @@ static selinux_enforcing_status selinux_status_from_cmdline() {
 
     return status;
 }
+#endif
 
 static bool selinux_is_enforcing(void)
 {
     if (ALLOW_PERMISSIVE_SELINUX) {
-        return selinux_status_from_cmdline() == SELINUX_ENFORCING;
+        return false;
     }
     return true;
 }
